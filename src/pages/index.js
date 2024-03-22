104daa3bf3b28154eac85815c9c7d736b515bcae
Tane4ka170 <tane4ka170@gmail.com>
2024-03-23 01:02:52 +0200

    finished layout and getStaticProps

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,21 @@
 import Head from "next/head";
 import styles from "../styles/home.module.css";
+import Layout from "@/components/Layout/Layout";
 
-export default function Home() {
+export default function Home({ countries }) {
   return (
-    
+    <Layout>
+      <div className={styles.counts}>Found {countries.length} countries</div>
+    </Layout>
   );
 }
+
+export const getStaticProps = async () => {
+  const res = await fetch("https://restcountries.com/v3.1/all");
+  const countries = await res.json();
+  return {
+    props: {
+      countries,
+    },
+  };
+};
