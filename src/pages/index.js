e90d31016c68faab183fea379de4de09dba6108d
Tane4ka170 <tane4ka170@gmail.com>
2024-03-24 01:36:15 +0200

    Build Country Table Component

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,14 @@ import Head from "next/head";
 import styles from "../styles/home.module.css";
 import Layout from "@/components/Layout/Layout";
 import SearchInput from "@/components/SearchInput/SearchInput";
+import CountriesTable from "@/components/CountriesTable/CountriesTable";
 
 export default function Home({ countries }) {
   return (
     <Layout>
       <div className={styles.counts}>Found {countries.length} countries</div>
       <SearchInput color="inherit" />
+      <CountriesTable countries={countries} />
     </Layout>
   );
 }
