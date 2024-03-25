193f5ed0c91413928da85173a547748fd152a84d
Tane4ka170 <141357975+Tane4ka170@users.noreply.github.com>
2024-03-25 18:55:06 +0200

    started styling countries

--- src/pages/country/[id].js
+++ src/pages/country/[id].js
@@ -1,12 +1,19 @@
+import Layout from "@/components/Layout/Layout";
+
 const Country = ({ country }) => {
-  console.log(country);
-  return <div>Country</div>;
+  return (
+    <Layout title={country.name}>
+      <div>
+        <div className={StylesProvider.overview}></div>
+      </div>
+    </Layout>
+  );
 };
 
 export default Country;
 
 export const getServerSideProps = async ({ params }) => {
-  const res = await fetch(`https://restcountries.com/v3.1/all/${params.cca3}`);
+  const res = await fetch(`https://restcountries.com/v3.1/all/${params.cioc}`);
   const country = await res.json();
   return {
     props: {
